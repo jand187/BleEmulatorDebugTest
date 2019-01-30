36694bb342130849651f1f93608da2fe38b8973b
Jan Daniel Andersen (JAXX) <jaxx@oticon.com>
2019-01-30 12:22:33 +0100

    first

---
+++ ClientConsole/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Linq;
+using System.ServiceModel;
+using SimpleService;
+
+namespace ClientConsole
+{
+    public class Program
+    {
+        private static readonly Uri baseAddress;
+
+        static Program()
+        {
+            baseAddress = new Uri("http://localhost:62363/DetectionService.svc");
+        }
+
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Press any key to call service");
+            Console.ReadKey();
+
+            var factory = new ChannelFactory<IDetectionService>(new BasicHttpBinding());
+            var endpointAddress = new EndpointAddress(baseAddress);
+            var channel = factory.CreateChannel(endpointAddress);
+
+            var instruments = channel.DetectInstruments("ab");
+
+            instruments.ToList().ForEach(Console.Write);
+        }
+    }
+}
---
+++ SimpleService/DetectionService.svc.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+
+namespace SimpleService
+{
+    [ServiceContract]
+    public interface IDetectionService
+    {
+        [OperationContract]
+        IEnumerable<string> DetectInstruments(string startsWith);
+    }
+
+
+    public class DetectionService : IDetectionService
+    {
+        public IEnumerable<string> DetectInstruments(string startsWith)
+        {
+            return new[]
+            {
+                "abe",
+                "abekat",
+                "missekat",
+                "kattemis",
+                "fedebadeabe",
+            }.Where(s => s.StartsWith(startsWith));
+        }
+    }
+}
